43dd5fd861b055acf115ceb51529851af91a14b5
Aswin K R <116720043+AlphaSpider@users.noreply.github.com>
2025-11-08 02:47:37 +0530

    First commit

---
+++ 1basics.js
@@ -0,0 +1,9 @@
+var myName="Aswin"
+var myStr="My name is "+ myName + " and I'm well!"
+console.log(myStr)
+
+namelen=myName.length;
+// secLetter=myName[1];
+// console.log(secLetter);
+// var lastLetter=myName[myName.length -1];
+console.log(lastLetter);
---
+++ 2arrays.js
@@ -0,0 +1,7 @@
+// var myArray=["Steven", 23]
+// console.log(myArray[1])
+
+// ------------Nested Arrays---------
+// when one of the elements in array is an array that is called a nested array
+var nArray=[["the universe", 43], ["galaxy", 22]]
+console.log(nArray[0],nArray[1])
