7fce02664b5059909e4e7143cacc5ed26ee8f114
Aswin K R <116720043+AlphaSpider@users.noreply.github.com>
2025-11-08 22:20:02 +0530

    Dusting off js halfway through

--- 2arrays.js
+++ 2arrays.js
@@ -1,7 +1,14 @@
-// var myArray=["Steven", 23]
+var myArray=["Steven", 23]
 // console.log(myArray[1])
 
 // ------------Nested Arrays---------
 // when one of the elements in array is an array that is called a nested array
 var nArray=[["the universe", 43], ["galaxy", 22]]
+// accessing elements in an array with indexes
 console.log(nArray[0],nArray[1])
+// modifying array data with indexes
+myArray[1]="Hawkins"
+console.log(myArray)
+var Arr=[[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]]
+console.log(Arr[3][0][0])
+
---
+++ 3mnplte-arr.js
@@ -0,0 +1,17 @@
+// manipulate array with push()
+var Arr=["Simpson", "got", "Cat" ]
+Arr.push(["at", "his", "home"])
+console.log(Arr)
+
+var newArr = [["john", 23], ["cat", 2]]
+newArr.push(["dog", 23])
+console.log(newArr)
+
+// manipulate array with pop()
+var ourArray=[1,2,3];
+var newArr2=ourArray.pop()
+console.log(newArr2)
+
+var strArr=[["John", 23], ["cat", 2]]
+varnewstrAr=strArr.pop()
+console.log(strArr)
