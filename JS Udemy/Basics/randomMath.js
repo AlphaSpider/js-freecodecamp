6e193917bb3d6a9ecd06bda739b51f5bad634f26
Aswin K R <116720043+AlphaSpider@users.noreply.github.com>
2025-11-29 23:50:48 +0530

    randommath

---
+++ JS Udemy/Basics/randomMath.js
@@ -0,0 +1,2 @@
+console.log(Math.floor(44.9999999))
+console.log(Math.ceil(44.111))
---
+++ JS Udemy/xFiles/basics/app.js
@@ -0,0 +1,21 @@
+const day=prompt('Enter a day!').toLowerCase();
+
+if(day=='monday'){
+    console.log("I love mon!");
+}else if(day=='thursday'){
+    console.log("I love thr");
+}else{
+    console.log("Nah!")
+}
+
+if(phrase=='stop'){
+console.log("red");
+}
+else if(phrase=='slow'){
+console.log("yellow");
+}
+else if(phrase=='go'){
+console.log("green");
+}else {
+console.log("purple");
+}
