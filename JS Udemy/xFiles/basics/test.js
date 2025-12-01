dca997c8f7520133c404dd27576d8a85016c58cd
Aswin K R <116720043+AlphaSpider@users.noreply.github.com>
2025-12-01 23:54:40 +0530

    test

---
+++ JS Udemy/xFiles/basics/test.js
@@ -0,0 +1,15 @@
+// A small JavaScript program
+
+// Function to greet the user
+function greet(name) {
+  console.log(`Hello, ${name}! Welcome to JavaScript.`);
+}
+
+// Function to add two numbers
+function add(a, b) {
+  return a + b;
+}
+
+// Run the program
+greet("Ashwin");
+console.log("2 + 3 =", add(2, 3));
